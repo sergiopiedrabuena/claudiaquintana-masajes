f46ba91594a616c3245de5c0c4e4078030966b2a
serghi.p <83840254+serghip@users.noreply.github.com>
2022-05-10 11:14:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-    //utilizar el heigth de pantalla del usuaria para definir el height del div imgPortada
+    $("body").queryLoader2({
+        barColor: "#555566",
+        backgroundColor: "#fff",
+        percentage: true,
+        barHeight: 1,
+        completeAnimation: "grow",
+        minimumTime: 100,
+        onLoadComplete: hidePreLoader
+      });
+      
+      function hidePreLoader() {
+        $("#precarga").hide();
+      }
+
+    //utilizar el heigth de pantalla del usuario para definir el height
     var height = $(window).height();
-          $('#cajaPortada').height(height);
-          //utilizar el heigth de pantalla del usuaria para definir el height del div cajaTrabajo
-          $('#cajaTextoPortada').height(height-(height/3));
-          $('#fondoTextoPortada').height(height/3);
-          $('#titulo').css('margin-top',height-(height/3));          
+        //calculos y distribucion del height en base a la pantalla del usuario
+        $('#cajaPortada').height(height);
+        $('#cajaTextoPortada').height(height-(height/3));
+        $('#fondoTextoPortada').height(height/3);
+        $('#titulo').css('margin-top',height-(height/3));    
 });
