234fa2b740e6390f8d80aba53d1f9d1ce5e7160d
serghi.p <83840254+serghip@users.noreply.github.com>
2022-05-10 11:40:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+    $( "body" ).slideDown( );
     $("body").queryLoader2({
-        barColor: "#555566",
-        backgroundColor: "#fff",
+        barColor: "orange",
+        backgroundColor: "grey",
         percentage: true,
         barHeight: 1,
         completeAnimation: "grow",
@@ -11,8 +12,9 @@ $(document).ready(function(){
       
       function hidePreLoader() {
         $("#precarga").hide( 500 ).delay( 1800 );
+        $( "body" ).slideUp( 300 ).delay( 500 ).fadeIn( 400 );
       }
-      $( "body" ).slideUp( 300 ).delay( 500 ).fadeIn( 400 );
+      
 
     //utilizar el heigth de pantalla del usuario para definir el height
     var height = $(window).height();
