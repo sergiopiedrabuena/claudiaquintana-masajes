ce8b43fcdd980985108c68cc0376673f03b50212
serghi.p <83840254+serghip@users.noreply.github.com>
2022-05-10 11:06:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ $(document).ready(function(){
           //utilizar el heigth de pantalla del usuaria para definir el height del div cajaTrabajo
           $('#cajaTextoPortada').height(height-(height/3));
           $('#fondoTextoPortada').height(height/3);
-          $('#titulo').css('margin-top',height-(height/3));
-          
+          $('#titulo').css('margin-top',height-(height/3));          
 });
